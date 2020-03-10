67a7f45fb6b3088be84699a0b76f50bb88b95984
pieper207 <60915570+pieper207@users.noreply.github.com>
2020-03-10 09:14:17 +0100

    fixed bugs

--- timelib.c
+++ timelib.c
@@ -7,19 +7,19 @@ int day_of_the_year(struct date indate)   // Die Funktion berechnet für ein gege
     int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
     int i;
     int ergebnis = 0;
-    int schaltjahr;
-    schaltjahr = is_leapyear(indate.year);
+    int leapyear;
+    leapyear = is_leapyear(indate);
 
-    if (schaltjahr == 1)        // Abfrage ob das Jahr ein Scahltjahr ist, damit in der folgenden Schleife kein Tag fehlt/ zu viel berechnet wird.
+    if (leapyear == 1)        // Abfrage ob das Jahr ein Scahltjahr ist, damit in der folgenden Schleife kein Tag fehlt/ zu viel berechnet wird.
     {
         days[1] = 29;
     }
-    for(i = 0; i < date.month -1; i++)
+    for(i = 0; i < indate.month -1; i++)
     {
         ergebnis += days[i];    // Bei jedem Durchlauf werden die Tage des Monats auf 'ergebnis' addiert, aber nur bis zum vorherigen Monat des
                                 // eingegebenen Monats.
     }
-    ergebnis += date.day;        // Nun wird nur noch die Tag-Anzahl (die zu Begin eigegeben wurde) auf 'ergebnis' addiert.
+    ergebnis += indate.day;        // Nun wird nur noch die Tag-Anzahl (die zu Begin eigegeben wurde) auf 'ergebnis' addiert.
 
     return ergebnis;
 }
@@ -27,20 +27,20 @@ int day_of_the_year(struct date indate)   // Die Funktion berechnet für ein gege
 
 int is_leapyear(struct date indate)   // Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des gregorianischen Kalender ein Schaltjahr ist
 {
-    if ((date.year % 4 == 0) && (date.year % 100 == 0) && (date.year % 400 == 0))
+    if ((indate.year % 4 == 0) && (indate.year % 100 != 0) || (indate.year % 400 == 0))
     {
-        date.year = 1;
+        indate.year = 1;
     }
-    if(date.year < 1582)
+    else if(indate.year < 1582)
     {
-        date.year = -1;
+        indate.year = -1;
     }
     else
     {
-        date.year = 0;
+        indate.year = 0;
     }
 
-    return date.year;
+    return indate.year;
 }
 
 int get_days_for_month(struct date indate) // Die Funktion bestimmt für einen gegebenen Monat eines gegebenen Jahres, wie viele Tage der Monat hat.
@@ -49,14 +49,14 @@ int get_days_for_month(struct date indate) // Die Funktion bestimmt für einen ge
     int i;
     int resultMonth = 0;
     int leapYear;
-    leapYear = is_leapyear(date.year);
+    leapYear = is_leapyear(indate);
 
     if (leapYear == 1)
     {
         days[1] = 29;
     }
 
-    for(i = 0; i < date.month ; i++)      // Die Schleife zählt 'i' solange hoch, bis sie beim aktuellen Monat angekommen ist.
+    for(i = 0; i < indate.month ; i++)      // Die Schleife zählt 'i' solange hoch, bis sie beim aktuellen Monat angekommen ist.
     {
         resultMonth = days[i];       // Die Tage des aktuellen Monats werden in 'resultMonth' geschrieben.
     }
@@ -69,20 +69,9 @@ int exists_date(struct date indate)  // Die Funktion überprüft, ob ein eingegebe
     int i;
     int valid;
     int days[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int daysOfMonth = get_days_for_month(indate);
 
-   for(i = 0; i < indate.month; i++)
-    {
-        if(indate.day > days[i])
-        {
-            valid = 0;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    if((indate.month < 1) || (indate.day < 1) || (indate.year < 1582) || (indate.year > 2400) || (indate.month > 12))     // Abfrage zur Gültigkeit des Datums.
+    if((indate.month < 1) || (indate.day < 1) || (indate.year < 1582) || (indate.year > 2400) || (indate.month > 12) || (indate.day > daysOfMonth))     // Abfrage zur Gültigkeit des Datums.
     {
         valid = 0;
     }
@@ -94,25 +83,27 @@ int exists_date(struct date indate)  // Die Funktion überprüft, ob ein eingegebe
     return valid;
 }
 
-void input_date(struct date *indate) // Zeigt auf die Stelle im Hauptprogramm.
+struct date input_date() // Zeigt auf die Stelle im Hauptprogramm.
 {                                                // Die Funktion liest 3 Ganzzahlwerte (Integer) ein, für Tag, Monat und Jahr. Wenn das angegebene
-                                                // Datum ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
+    struct date indate;
+                                             // Datum ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
 
     int trueOrFalse;
 
     do // Wenn das Datum ungültig ist, wird es erneut eingegeben.
     {
-    printf("Bitte geben Sie den Tag und den Monat ohne eine 0 davor ein!\n\n"); //Hinweis zur korrekten Eingabe.
+    printf("Bitte geben Sie den Tag und den Monat als Zahl an!\n\n"); //Hinweis zur korrekten Eingabe.
     printf("Bitte Tag eingeben: \n");           //Eingabe des Tages.
-    scanf("%d", day);                           // kein & benötigt das dieses beim Übergeben der Variablen verwendet wird und ein Zeiger im Unterprogramm darauf zeigt.
+    scanf("%d", &(indate.day));                           // kein & benötigt das dieses beim Übergeben der Variablen verwendet wird und ein Zeiger im Unterprogramm darauf zeigt.
 
     printf("Bitte Monat eingeben: \n");         // Eingabe des Monats.
-    scanf("%d", month);
+    scanf("%d", &(indate.month));
 
     printf("Bitte Jahr eingeben: \n");          // Eingabe des Jahres.
-    scanf("%d", year);
+    scanf("%d", &(indate.year));
 
-    trueOrFalse = exists_date(*day, *month, *year); // Das Unterprogramm 'exists_date' liefert die Gültigkeit und wird in 'trueOrFalse' geschrieben.
+    trueOrFalse = exists_date(indate); // Das Unterprogramm 'exists_date' liefert die Gültigkeit und wird in 'trueOrFalse' geschrieben.
     }while(trueOrFalse == 0);
+    return indate;
 }
 
